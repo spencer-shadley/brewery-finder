51e6d607d7f98e4dd41eda8a895210ab0cb3b82c
Richard Wang <richardchacha@hotmail.com>
2020-01-11 16:37:38 -0800

    add locate me button, clear up functions

---
+++ test-objects.js
@@ -0,0 +1,33 @@
+let breweryObj = [
+    {
+        "id": 7646,
+        "name": "Rock Wood Fired Pizza and Brewery - Lynnwood",
+        "brewery_type": "contract",
+        "street": "4010 196th St SW",
+        "city": "Lynnwood",
+        "state": "Washington",
+        "postal_code": "98036-6715",
+        "country": "United States",
+        "longitude": "-122.287641610199",
+        "latitude": "47.82048455",
+        "phone": "4256976007",
+        "website_url": "http://www.therockwfp.com",
+        "updated_at": "2018-08-24T16:38:50.629Z",
+        "tag_list": []
+    }
+    , {
+        "id": 7438,
+        "name": "Ellersick Brewing/ Big E Ales",
+        "brewery_type": "brewpub", "street": "5030 208th St SW Ste A",
+        "city": "Lynnwood",
+        "state": "Washington",
+        "postal_code": "98036-7642",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "4256727051",
+        "website_url": "http://www.bigeales.com",
+        "updated_at": "2018-08-11T21:40:15.270Z",
+        "tag_list": []
+    }
+]
