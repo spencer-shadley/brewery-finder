290c6edac75b9b96e776ff2401cdb2b33b6431ef
Richard Wang <richardchacha@hotmail.com>
2020-01-12 14:21:14 -0800

    revise html and add function to update brewery list

--- test-objects.js
+++ test-objects.js
@@ -1,33 +1,43 @@
 let breweryObj = [
     {
-        "id": 7646,
-        "name": "Rock Wood Fired Pizza and Brewery - Lynnwood",
-        "brewery_type": "contract",
-        "street": "4010 196th St SW",
-        "city": "Lynnwood",
-        "state": "Washington",
-        "postal_code": "98036-6715",
-        "country": "United States",
-        "longitude": "-122.287641610199",
-        "latitude": "47.82048455",
-        "phone": "4256976007",
-        "website_url": "http://www.therockwfp.com",
-        "updated_at": "2018-08-24T16:38:50.629Z",
-        "tag_list": []
-    }
-    , {
-        "id": 7438,
-        "name": "Ellersick Brewing/ Big E Ales",
-        "brewery_type": "brewpub", "street": "5030 208th St SW Ste A",
-        "city": "Lynnwood",
-        "state": "Washington",
-        "postal_code": "98036-7642",
-        "country": "United States",
-        "longitude": null,
-        "latitude": null,
-        "phone": "4256727051",
-        "website_url": "http://www.bigeales.com",
-        "updated_at": "2018-08-11T21:40:15.270Z",
-        "tag_list": []
+        "id": 3000, "name": "D.L. Geary Brewing Co Inc.", "brewery_type": "micro", "street": "38 Evergreen Dr", "city": "Portland", "state": "Maine", "postal_code": "04103-1066", "country": "United States", "longitude": "-70.314848130679", "latitude": "43.7076741571514", "phone": "2078782337", "website_url": "http://www.gearybrewing.com", "updated_at": "2018-08-24T00:39:39.267Z", "tag_list": []
+    }, {
+        "id": 3021, "name": "Gritty McDuffs - Portland", "brewery_type": "brewpub", "street": "396 Fore St", "city": "Portland", "state": "Maine", "postal_code": "04101-4026", "country": "United States", "longitude": "-70.2533609", "latitude": "43.6564108", "phone": "2077722739", "website_url": "http://www.grittys.com", "updated_at": "2018-08-24T00:39:48.759Z", "tag_list": []
+    }, {
+        "id": 3075, "name": "Shipyard Brewing Co", "brewery_type": "regional", "street": "86 Newbury St", "city": "Portland", "state": "Maine", "postal_code": "04101-4219", "country": "United States", "longitude": "-70.2486231394133", "latitude": "43.6615851", "phone": "2077610807", "website_url": "http://www.shipyard.com", "updated_at": "2018-08-24T00:40:11.302Z", "tag_list": []
+    }, {
+        "id": 3478, "name": "ConfluxCity Brewing Company", "brewery_type": "micro", "street": "110 N Water St", "city": "Portland", "state": "Michigan", "postal_code": "48875", "country": "United States", "longitude": "-84.9032148381293", "latitude": "42.8714242896063", "phone": "5175269091", "website_url": "http://www.confluxcitybrew.com", "updated_at": "2018-08-24T00:42:37.634Z", "tag_list": []
+    }, {
+        "id": 5711, "name": "Arlington Club", "brewery_type": "micro", "street": "811 SW Salmon St", "city": "Portland", "state": "Oregon", "postal_code": "97205-3094", "country": "United States", "longitude": "-122.681859641942", "latitude": "45.51786855", "phone": "5032234141", "website_url": "http://www.thearlingtonclub.com", "updated_at": "2018-08-24T15:45:50.469Z", "tag_list": []
+    }, {
+        "id": 5758, "name": "Cascade Brewing Barrel House", "brewery_type": "micro", "street": "939 SE Belmont St", "city": "Portland", "state": "Oregon", "postal_code": "97214-2519", "country": "United States", "longitude": "-122.656005749976", "latitude": "45.5167536", "phone": "5032658603", "website_url": "http://www.cascadebrewing.com", "updated_at": "2018-08-24T15:46:08.535Z", "tag_list": []
+    }, {
+        "id": 5933, "name": "Scout Beer", "brewery_type": "micro", "street": "1516 SE 10th Ave", "city": "Portland", "state": "Oregon", "postal_code": "97214-3585", "country": "United States", "longitude": "-122.655590489796", "latitude": "45.5120189183673", "phone": "5032064735", "website_url": "http://www.scoutpdx.com", "updated_at": "2018-08-24T15:47:12.443Z", "tag_list": []
+    }, {
+        "id": 5805, "name": "Full Sail Brewery - Riverplace", "brewery_type": "micro", "street": "0307 SW Montgomery St", "city": "Portland", "state": "Oregon", "postal_code": "97201-5125", "country": "United States", "longitude": "-122.673521447995", "latitude": "45.50957205", "phone": "5032225343", "website_url": "http://www.fullsailbrewing.com", "updated_at": "2018-08-24T16:01:44.127Z", "tag_list": []
+    }, {
+        "id": 5837, "name": "Laurelwood Public House \u0026 Brewery - SE Portland", "brewery_type": "brewpub", "street": "6716 SE Milwaukie Ave", "city": "Portland", "state": "Oregon", "postal_code": "97202-5618", "country": "United States", "longitude": "-122.64849505", "latitude": "45.47415655", "phone": "5039436157", "website_url": "", "updated_at": "2018-08-24T16:02:29.973Z", "tag_list": []
+    }, {
+        "id": 5867, "name": "McMenamins Oak Hills Brewery", "brewery_type": "micro", "street": "14740 NW Cornell Rd Ste 80", "city": "Portland", "state": "Oregon", "postal_code": "97229-5400", "country": "United States", "longitude": "-122.7105079", "latitude": "45.5330425", "phone": "5036450117", "website_url": "http://www.mcmenamins.com", "updated_at": "2018-08-24T16:03:12.648Z", "tag_list": []
+    }, {
+        "id": 5970, "name": "Vanport Brewing Company", "brewery_type": "planning", "street": "", "city": "Portland", "state": "Oregon", "postal_code": "97211-2907", "country": "United States", "longitude": null, "latitude": null, "phone": "5032670219", "website_url": "", "updated_at": "2018-08-11T21:39:20.562Z", "tag_list": []
+    }, {
+        "id": 2980, "name": "Austin Street Brewery", "brewery_type": "micro", "street": "1 Industrial Way Ste 8", "city": "Portland", "state": "Maine", "postal_code": "04103-1072", "country": "United States", "longitude": null, "latitude": null, "phone": "2072001994", "website_url": "http://www.austinstreetbrewery.com", "updated_at": "2018-08-11T21:37:30.384Z", "tag_list": []
+    }, {
+        "id": 2987, "name": "Battery Steele Brewing", "brewery_type": "micro", "street": "1 Industrial Way Ste 12", "city": "Portland", "state": "Maine", "postal_code": "04103-1072", "country": "United States", "longitude": null, "latitude": null, "phone": "", "website_url": "http://www.batterysteele.com", "updated_at": "2018-08-11T21:37:30.649Z", "tag_list": []
+    }, {
+        "id": 2976, "name": "Allagash Brewing Co", "brewery_type": "regional", "street": "50 Industrial Way", "city": "Portland", "state": "Maine", "postal_code": "04103-1270", "country": "United States", "longitude": "-70.3179518", "latitude": "43.7030951", "phone": "2078785385", "website_url": "http://www.allagash.com", "updated_at": "2018-08-24T00:39:29.904Z", "tag_list": []
+    }, {
+        "id": 2998, "name": "Bunker Brewing Co", "brewery_type": "micro", "street": "17 Westfield St Unit D", "city": "Portland", "state": "Maine", "postal_code": "04102-2730", "country": "United States", "longitude": null, "latitude": null, "phone": "2074505014", "website_url": "http://www.bunkerbrewingco.com", "updated_at": "2018-08-11T21:37:31.175Z", "tag_list": []
+    }, {
+        "id": 3010, "name": "Foundation Brewing Company", "brewery_type": "micro", "street": "1 Industrial Way Ste 5", "city": "Portland", "state": "Maine", "postal_code": "04103-1072", "country": "United States", "longitude": null, "latitude": null, "phone": "2073708187", "website_url": "http://www.foundationbrew.com", "updated_at": "2018-08-11T21:37:31.775Z", "tag_list": []
+    }, {
+        "id": 3017, "name": "Goodfire Brewing Company", "brewery_type": "micro", "street": "219 Anderson St Ste 6", "city": "Portland", "state": "Maine", "postal_code": "04101-1401", "country": "United States", "longitude": null, "latitude": null, "phone": "2078088910", "website_url": "http://www.goodfirebrewing.com", "updated_at": "2018-08-11T21:37:32.021Z", "tag_list": []
+    }, {
+        "id": 3032, "name": "Liquid Riot Bottling Co.", "brewery_type": "brewpub", "street": "4 Canal Plz Ste 1", "city": "Portland", "state": "Maine", "postal_code": "04101-4594", "country": "United States", "longitude": null, "latitude": null, "phone": "2072218889", "website_url": "http://www.novareresbiercafe.com", "updated_at": "2018-08-11T21:37:32.643Z", "tag_list": []
+    }, {
+        "id": 2993, "name": "Bissell Brothers Brewing", "brewery_type": "micro", "street": "4 Thompsons Pt Ste 108", "city": "Portland", "state": "Maine", "postal_code": "04102-2639", "country": "United States", "longitude": "-70.2904097", "latitude": "43.6514062", "phone": "2074233622", "website_url": "http://www.bissellbrothers.com", "updated_at": "2018-08-24T00:39:36.895Z", "tag_list": []
+    }, {
+        "id": 3008, "name": "Fore River Brewing Company", "brewery_type": "micro", "street": "45 Huntress Ave", "city": "South Portland", "state": "Maine", "postal_code": "04106-4115", "country": "United States", "longitude": "-70.2876710514231", "latitude": "43.6324906649993", "phone": "2073700629", "website_url": "http://www.foreriverbrewing.com", "updated_at": "2018-08-24T00:39:42.896Z", "tag_list": []
     }
 ]
